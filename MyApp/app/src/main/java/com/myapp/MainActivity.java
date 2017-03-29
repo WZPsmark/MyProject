c0ab562846ffae337044537a4e11f165e8b70dee
smark <smarkwzp@163.com>
2017-03-29 20:58:29 +0800

    android pay

--- MyApp/app/src/main/java/com/myapp/MainActivity.java
+++ MyApp/app/src/main/java/com/myapp/MainActivity.java
@@ -6,6 +6,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import io.github.mayubao.pay_library.AliPayAPI;
+import io.github.mayubao.pay_library.AliPayReq;
+import io.github.mayubao.pay_library.PayAPI;
+import io.github.mayubao.pay_library.WechatPayReq;
+
 
 /**
  * WIFI扫描与连接
@@ -47,6 +52,67 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+
+    /**
+     * 微信支付例子
+     */
+    private void wXpay(){
+//        WechatPayReq wechatPayReq = new WechatPayReq.Builder()
+//                .with(this) //activity实例
+//                .setAppId(appid) //微信支付AppID
+//                .setPartnerId(partnerid)//微信支付商户号
+//                .setPrepayId(prepayid)//预支付码
+////                              .setPackageValue(wechatPayReq.get)//"Sign=WXPay"
+//                .setNonceStr(noncestr)
+//                .setTimeStamp(timestamp)//时间戳
+//                .setSign(sign)//签名
+//                .create();
+//        //2.发送微信支付请求
+//        PayAPI.getInstance().sendPayRequest(wechatPayReq);
+        //关于微信支付的回调
+        //wechatPayReq.setOnWechatPayListener(new OnWechatPayListener);
+    }
+
+
+    /**
+     * 支付宝支付
+     */
+    private void zhiFubaoPAy(){
+
+        //appId:2017032906470394
+//        AliPayAPI.Config config = new AliPayAPI.Config.Builder()
+//                .setRsaPrivate(rsa_private) //设置私钥 (商户私钥，pkcs8格式)
+//                .setRsaPublic(rsa_public)//设置公钥(// 支付宝公钥)
+//                .setPartner(partner) //设置商户
+//                .setSeller(seller) //设置商户收款账号
+//                .create();
+
+        //2.创建支付宝支付请求
+//        AliPayReq aliPayReq = new AliPayReq.Builder()
+//                .with(this)//Activity实例
+//                .apply(config)//支付宝支付通用配置
+//                .setOutTradeNo(outTradeNo)//设置唯一订单号
+//                .setPrice(price)//设置订单价格
+//                .setSubject(orderSubject)//设置订单标题
+//                .setBody(orderBody)//设置订单内容 订单详情
+//                .setCallbackUrl(callbackUrl)//设置回调地址
+//                .create()//
+//                .setOnAliPayListener(null);//
+
+        //3.发送支付宝支付请求
+//        PayAPI.getInstance().sendPayRequest(aliPayReq);
+
+        //关于支付宝支付的回调
+        //aliPayReq.setOnAliPayListener(new OnAliPayListener);
+
+
+
+    }
+
+
+
+
 
 
 }
